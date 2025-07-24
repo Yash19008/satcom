486e28dc47425adaf643bf825894494b3b648e7f
Yash Soni <yash190068@gmail.com>
2025-07-24 21:50:51 +0530

    Third Commit

---
+++ include/partner_logo.php
@@ -0,0 +1,27 @@
+<div class="slider mb-1 container">
+    <div class="wrapper">
+        <i id="left" class="fa-solid fa-arrow-left"></i>
+        <div class="carousel">
+            <img src="./assets/images/partner-logo/1.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/2.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/3.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/4.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/5.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/6.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/7.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/8.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/9.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/10.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/11.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/12.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/13.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/14.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/15.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/16.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/17.png" alt="img" draggable="false">
+            <img src="./assets/images/partner-logo/18.png" alt="img" draggable="false">
+        </div>
+        <i id="right" class="fa-solid fa-arrow-right"></i>
+    </div>
+    <div class="dots-container"></div>
+</div>
